278e3c6759563828243d8526620168775e074c2f
nriosgamboa <165716839+nriosgamboa@users.noreply.github.com>
2025-10-06 23:16:17 -0300

    Update config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const API_URL = import.meta.env.VITE_API_URL || 'https://chat-bot-dk96.onrender.com';
+const WS_URL = import.meta.env.VITE_WS_URL || 'wss://chat-bot-dk96.onrender.com';
 
-export {
-  API_URL
-};
+export { API_URL, WS_URL };
